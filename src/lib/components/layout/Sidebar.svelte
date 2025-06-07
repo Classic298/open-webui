c80eb6c2237f5f9f9681b5e78e0d07b59107c233
Classic298 <27028174+Classic298@users.noreply.github.com>
2025-06-07 22:45:49 +0200

    Update Sidebar.svelte

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -219,7 +219,9 @@
 	};
 
 	const importChatHandler = async (items, pinned = false, folderId = null) => {
+		console.log('importChatHandler', items, pinned, folderId);
 		for (const item of items) {
+			console.log(item);
 			if (item.chat) {
 				await importChat(localStorage.token, item.chat, item?.meta ?? {}, pinned, folderId);
 			}
@@ -229,6 +231,8 @@
 	};
 
 	const inputFilesHandler = async (files) => {
+		console.log(files);
+
 		for (const file of files) {
 			const reader = new FileReader();
 			reader.onload = async (e) => {
@@ -247,6 +251,7 @@
 	};
 
 	const tagEventHandler = async (type, tagName, chatId) => {
+		console.log(type, tagName, chatId);
 		if (type === 'delete') {
 			initChatList();
 		} else if (type === 'add') {
@@ -273,12 +278,14 @@
 
 	const onDrop = async (e) => {
 		e.preventDefault();
+		console.log(e); // Log the drop event
 
 		// Perform file drop check and handle it accordingly
 		if (e.dataTransfer?.files) {
 			const inputFiles = Array.from(e.dataTransfer?.files);
 
 			if (inputFiles && inputFiles.length > 0) {
+				console.log(inputFiles); // Log the dropped files
 				inputFilesHandler(inputFiles); // Handle the dropped files
 			}
 		}
@@ -304,6 +311,7 @@
 
 	const onTouchStart = (e) => {
 		touchstart = e.changedTouches[0];
+		console.log(touchstart.clientX);
 	};
 
 	const onTouchEnd = (e) => {
@@ -767,6 +775,7 @@
 						}
 
 						if (chat) {
+							console.log(chat);
 							if (chat.folder_id) {
 								const res = await updateChatFolderIdById(localStorage.token, chat.id, null).catch(
 									(error) => {
@@ -811,6 +820,7 @@
 							bind:open={showPinnedChat}
 							on:change={(e) => {
 								localStorage.setItem('showPinnedChat', e.detail);
+								console.log(e.detail);
 							}}
 							on:import={(e) => {
 								importChatHandler(e.detail, true);
@@ -827,6 +837,7 @@
 									}
 
 									if (chat) {
+										console.log(chat);
 										if (chat.folder_id) {
 											const res = await updateChatFolderIdById(
 												localStorage.token,
